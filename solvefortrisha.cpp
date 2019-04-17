5d9e39fc8e9996c3c9f27184828321bce50ba0df
Tanuj Jain <tanujjain244@yahoo.com>
2019-04-17 16:40:40 +0530

    Create solvefortrisha.cpp

---
+++ solvefortrisha.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+class SolveForTrisha
+    {
+    public:
+    int findSolution(int n)
+        {
+        if(n%2==0)
+            return 0;
+        else 
+            return n;
+    }
+};
